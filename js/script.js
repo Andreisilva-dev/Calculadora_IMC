957d53c8e98eab396674e906fb23b403b51703b7
Andreisilva-dev <andrei765silva@gmail.com>
2025-02-11 10:33:25 -0300

    calculadora finalizada

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+// pegando os inputs:
+let formulario = document.querySelector('#formulario')
+let CaixaNome = document.querySelector('#nome')
+let CaixaIdade = document.querySelector('#idade')
+let CaixaPeso = document.querySelector('#peso')
+let CaixaAltura = document.querySelector('#altura')
+let CaixaImc = document.querySelector('#resultadoImc')
+
+let dados = document.querySelectorAll('.pessoa')
+let aviso = document.querySelector('#aviso')
+
+// botao
+
+let btnEnviar = document.querySelector('#btnEnviar')
+
+btnEnviar.addEventListener('click', function(e){
+    let nome = CaixaNome.value
+    let idade = Number(CaixaIdade.value)
+    let peso = parseFloat(CaixaPeso.value)
+    let altura = parseFloat(CaixaAltura.value)
+    let imc = (peso / (altura * altura)).toFixed(1)
+
+    console.log(nome)
+    console.log(idade)
+    console.log(peso)
+    console.log(altura)
+    console.log(imc)
+
+    CaixaImc.value = imc
+    let sit = situacaoPeso(imc)
+    aviso.textContent = sit
+
+    let pessoa = {
+        nome    : nome,
+        idade   : idade,
+        peso    : peso,
+        altura  : altura,
+        imc     : imc
+    }
+
+    dados[1].textContent = nome 
+    dados[2].textContent = idade + ' anos'
+    dados[3].textContent = peso + ' kg'
+    dados[4].textContent = altura + ' m'
+    dados[5].textContent = 'Imc: '+ imc + ' ' + sit
+    
+    
+    e.preventDefault()
+})
+
+function situacaoPeso(imc){
+    let situacao = ''
+    if (imc >= 40){
+        situacao = 'Obesidade grau III'
+    } else if (imc >= 35 && imc <= 39.9){
+        situacao = 'Obesidade grau II'
+    } else if (imc >= 30 && imc <= 34.9) {
+        situacao = 'Obesidade grau I'
+    } else if (imc >= 25 && imc <= 29.9) {
+        situacao = 'Sobrepeso'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        situacao = 'Normal'
+    } else if (imc < 18.5){
+        situacao = 'Magreza'
+    } else {
+        situacao = 'erro'
+    }
+
+    return situacao
+}
+
